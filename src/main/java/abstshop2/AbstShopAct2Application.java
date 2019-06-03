828c1cf47c797915d8d16d52666f6151b05741a8
Nathan Hergenreter <herg2019@iastate.edu>
2019-06-03 13:35:30 -0500

    Initial commit

---
+++ src/main/java/abstshop2/AbstShopAct2Application.java
@@ -0,0 +1,13 @@
+package abstshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstShopAct2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstShopAct2Application.class, args);
+	}
+
+}
